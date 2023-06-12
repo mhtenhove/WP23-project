637334f326f3b94ff958af03b913fe88a50e457c
Jelmer <jelmersmit@upcmail.nl>
2023-06-12 13:14:03 +0200

    Removed old load.php and save.php that were not in script dir

--- load.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $json_file = file_get_contents("data/data.json");
-    $data = json_decode($json_file, true);
-    if (isset($_GET["attr"])) {
-        echo $data[$_GET["attr"]];
-    } else {
-        echo $json_file;
-    }
-?>
