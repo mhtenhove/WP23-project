f9ea1dda72c28ce32492dd7a888512166e0ab4b2
matthijs <m.h.ten.hove@student.rug.nl>
2023-06-13 19:42:51 +0200

    wrote script to change the identifier of the current user when clicking on the button

--- scripts/player_turn.php
+++ scripts/player_turn.php
@@ -1,6 +1,25 @@
 <?php
-if (isset($_POST['player-name'])) {
-    $json_file = file_get_contents("../data/data.json");
-    $players = json_decode($json_file, true);
+function p_print($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
 }
+
+$json_file = file_get_contents("../data/current_player.json");
+$current_player = json_decode($json_file, true);
+
+if ($current_player == [1]) {
+    $current_player = [2];
+    $updated_player = json_encode($current_player);
+    file_put_contents("../data/current_player.json", $updated_player);
+} else {
+    $current_player = [1];
+    $updated_player = json_encode($current_player);
+    file_put_contents("../data/current_player.json", $updated_player);
+}
+
+p_print($current_player);
+//header("Location: ../game.php");
+//exit()
+
 ?>
