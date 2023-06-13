52dc841deaca2f28eb283961171a914b4f2f76b1
matthijs <m.h.ten.hove@student.rug.nl>
2023-06-13 19:42:23 +0200

    changed game.php in order to print the username (not fully functional yet)

--- game.php
+++ game.php
@@ -19,19 +19,30 @@ include __DIR__ . '/tpl/body_start.php';
             <h1>Welcome to Blackjack!</h1>
             <strong>Press the button to get a card</strong>
             <p id="player-num"></p>
+            <form action="scripts/player_turn.php">
             <button class="btn btn-primary" id="test-btn">Click Here!</button>
+            </form>
         </div>
         <div class="row">
             <h1>
                 <?php
                 $json_file = file_get_contents("data/data.json");
                 $decoded_file = json_decode($json_file, true);
-                $current_player = $decoded_file[1];
+
+                $identifier_file = file_get_contents("data/current_player.json");
+                $identifier_decode = json_decode($identifier_file, true);
+                $identifier = $identifier_decode[0];
+
+                $current_player = $decoded_file[$identifier];
                 $current_player_name = $current_player['name'];
+
                 p_print("<div><p>The current player is:</p>");
                 p_print("<p id='current-player-info'>$current_player_name</p></div>");
                 ?>
             </h1>
+
+
+
         </div>
     </div>
 
