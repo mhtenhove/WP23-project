46ccb70e92182eeed9d7eb37e536814ad6bf5715
matthijs <m.h.ten.hove@student.rug.nl>
2023-06-14 20:37:05 +0200

    added extra button for extra card

--- game.php
+++ game.php
@@ -18,11 +18,15 @@ include __DIR__ . '/tpl/body_start.php';
     <div class="row">
         <div class="col-md-12">
             <h1>Welcome to Blackjack!</h1>
-            <strong>Press the button to get a card</strong>
+            <strong>Press the button if you want to finish your turn</strong>
             <p id="player-num"></p>
             <form action="scripts/player_turn.php">
-            <button name="test-btn" class="btn btn-primary" id="test-btn">Click Here!</button>
+            <button name="test-btn" class="btn btn-primary" id="test-btn">Switch turn</button>
             </form>
+
+            <strong>Press the button to get a card</strong><br/>
+            <button name="more-cards" class="btn btn-primary" id="more-cards">Give extra card!</button>
+
         </div>
         <div class="row">
             <h1>
@@ -32,8 +36,7 @@ include __DIR__ . '/tpl/body_start.php';
                 ?>
             </h1>
 
-
-            <img src="media/img/1.jpg"/>
+<!--            <img src="/media/img/1.jpg"/>-->
 
 
         </div>
