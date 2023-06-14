352cf9e234ccf2a58d1019f41ac86357497a465d
Ramon Jelsma <jelsmaramon@gmail.com>
2023-06-14 20:00:37 +0200

    Made player_turn.php compatible with more than 2 players

--- scripts/player_turn.php
+++ scripts/player_turn.php
@@ -11,22 +11,16 @@ $current_player_id = json_decode($json_file1, true);
 $players = json_decode($json_file2, true);
 $player_count = sizeof($players) - 1;
 
-foreach ($players as $key => $value) {
-    $player_id = $value['id'];
-}
-
-if ($current_player_id == $player_id) {
-    if ($current_player_id != $player_count) {
-        $current_player_id += 1;
-        $updated_player_id = json_encode($current_player_id);
-        $cur_play_file = fopen("../data/current_player.json", "w");
-        fwrite($cur_play_file, $updated_player_id);
-    } else {
-        $current_player = 1;
-        $updated_player = json_encode($current_player);
-        $cur_play_file = fopen("../data/current_player.json", "w");
-        fwrite($cur_play_file, $updated_player);
-    }
+if ($current_player_id != $player_count) {
+    $current_player_id += 1;
+    $updated_player_id = json_encode($current_player_id);
+    $cur_play_file = fopen("../data/current_player.json", "w");
+    fwrite($cur_play_file, $updated_player_id);
+} else {
+    $current_player = 1;
+    $updated_player = json_encode($current_player);
+    $cur_play_file = fopen("../data/current_player.json", "w");
+    fwrite($cur_play_file, $updated_player);
 }
 
 p_print($current_player_id);
