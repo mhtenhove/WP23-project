a5f4e64ddcc630d8ee67973512dacd9b17a4d992
Jelmer <jelmersmit@upcmail.nl>
2023-06-14 15:34:49 +0200

    Housekeeping in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,41 +4,19 @@ function player_join() {
         sessionStorage.setItem('player-name', playername);
     });
     
-    // Game function
-//     $("#test-btn").click(function(event) {
-//         $.ajax({
-//             url: '../scripts/save.php',
-//             method: 'POST',
-//             data: { 'player-name': sessionStorage.getItem('player-name') },
-//             success: function(){
-//                 load_player_info();
-//             },
-//         });
-//     });
-// }
     // Game function v.2
-        $("#test-btn").click(function(event) {
-            event.preventDefault();
-            $.ajax({
-                url: '../scripts/player_turn.php',
-                method: 'GET',
-                success: function(){
-                    //alert("dfsaffd");
-                },
-            });
+    $("#test-btn").click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: '../scripts/player_turn.php',
+            method: 'GET',
+            success: function(){
+                //alert("dfsaffd");
+            },
         });
+    });
 }
 
-// function load_player_info() {
-//     $.ajax({
-//         url: '../scripts/load.php',
-//         method: 'GET',
-//         data: { 'attr': 'name' },
-//         success: function(response){
-//             $("#current-player-info").html(response);
-//         },
-//     });
-// }
 
 function update_current_player() {
     $.ajax({
