c8293919fb4b68ba69223d275e9747385a9bf0bc
Jelmer <jelmersmit@upcmail.nl>
2023-06-15 15:09:05 +0200

    Made extra_card.php work

--- scripts/extra_card.php
+++ scripts/extra_card.php
@@ -1,4 +1,48 @@
 <?php
-echo("test");
+    function p_print($array){
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+    }
+    // load decks
+    $player_deck = Array();
+    $cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,
+                37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52];
+    // first, deal cards
+    $i = random_int(1, 52);
+    while (!in_array($i, $cards)) {
+        $i = random_int(1, 52);
+    }
+    if (in_array($i, $cards)) {
+        array_push($player_deck, $i);
+        array_splice($cards, $i, 1);
+    }
+    p_print($player_deck);
+    //p_print($player_deck);
 
+    // save
+    $score_json = file_get_contents("../data/scores.json");
+    $scores = json_decode($score_json, true);
+    p_print($scores);
+    
+    $current_player = file_get_contents("../data/current_player.json");
+    $player_score = Array(
+        $current_player => $scores[$current_player],
+    );
+    p_print($player_score);
+    
+    // save scores
+    $score = $player_score[$current_player];
+    $value_json = file_get_contents("../data/card_values.json");
+    $values = json_decode($value_json, true);
+    // get values
+    for ($x = 0; $x < count($values); $x++){
+        if ($values[$x]["card"] == $player_deck[0]) {
+            $score += $values[$x]["value"];
+        }
+    }
+    $player_score[$current_player] = $score;
+    $scorefile = fopen("../data/scores.json", "w");
+    fwrite($scorefile, json_encode($player_score));
+    
 ?>
