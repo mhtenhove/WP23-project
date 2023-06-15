8250a0b43fed394174e4691584001eb8fedcb899
Jelmer <jelmersmit@upcmail.nl>
2023-06-15 16:37:03 +0200

    Removed unnecessary p_print from load_current_user.php

--- scripts/load_current_user.php
+++ scripts/load_current_user.php
@@ -1,11 +1,5 @@
 <?php
 
-function p_print($array){
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
-}
-
 $json_file = file_get_contents("../data/current_player.json");
 $data = json_decode($json_file, true);
 
