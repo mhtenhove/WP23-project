d9e67b1fb9ea99392be10d1171d6e6e8b456c409
Ramon Jelsma <jelsmaramon@gmail.com>
2023-06-15 15:42:01 +0200

    Added serverside username validation.

--- scripts/player_counter.php
+++ scripts/player_counter.php
@@ -1,32 +1,44 @@
 <?php
 if (isset($_POST['join'])) {
-    $json_file = file_get_contents("../data/data.json");
-    $players = json_decode($json_file, true);
-    $player_id = 0;
-
-    foreach ($players as $key => $value) {
-        $player_id = $value['id'];
-    }
-    $player_id += 1;
-    if ($player_id == 1) {
-        array_push($players, [
-            'id' => $player_id,
-            'name' => $_POST['player-name'],
-            'status' => 'active'
-        ]);
-    } else if ($player_id > 1 and $player_id < 5) {
-        array_push($players, [
-            'id' => $player_id,
-            'name' => $_POST['player-name'],
-            'status' => 'inactive'
-        ]);
+    $playername = $_POST["player-name"];
+    if (empty($playername)) {
+        exit;
     }
 
-    $json_file = fopen('../data/data.json', 'w');
-    fwrite($json_file, json_encode($players));
-    fclose($json_file);
-    header("Location: ../game.php");
-    die();
+    else if (!preg_match('/^[a-zA-Z0-9]+$/', $playername)){
+        exit;
+    }
+
+    else {
+        $json_file = file_get_contents("../data/data.json");
+        $players = json_decode($json_file, true);
+        $player_id = 0;
+
+        foreach ($players as $key => $value) {
+            $player_id = $value['id'];
+        }
+        $player_id += 1;
+        if ($player_id == 1) {
+            array_push($players, [
+                'id' => $player_id,
+                'name' => $_POST['player-name'],
+                'status' => 'active'
+            ]);
+        } else if ($player_id > 1 and $player_id < 5) {
+            array_push($players, [
+                'id' => $player_id,
+                'name' => $_POST['player-name'],
+                'status' => 'inactive'
+            ]);
+        }
+
+        $json_file = fopen('../data/data.json', 'w');
+        fwrite($json_file, json_encode($players));
+        fclose($json_file);
+        header("Location: ../game.php");
+        die();
+    }
+
 }
     else {
         echo("<div><p>The maximum player count has been reached, try again later</p>");
