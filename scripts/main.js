a0ce560c1324fece81912214f151d21de7e951e5
Jelmer <jelmersmit@upcmail.nl>
2023-06-15 16:33:49 +0200

    Buttons are now blocked if it is not your turn

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,6 @@ function player_join() {
                 current_user_name = response;
                 if (current_user_name == user_name) {
                     // username ophalen uit current_player.json
-                    console.log("True");
                     $.ajax({
                         url: 'scripts/player_turn.php',
                         method: 'GET',
@@ -52,21 +51,33 @@ function player_join() {
     });
     $("#start-turn-btn").click(function(event) {
         event.preventDefault();
+        current_user_name = "";
+        user_name = sessionStorage.getItem('player-name');
         $.ajax({
-            url: 'scripts/start_game.php',
+            url: 'scripts/load_current_user.php',
             method: 'GET',
-            success: function(response){
-                card1 = response.split(" ")[0];
-                card2 = response.split(" ")[1];
-                card1_url = "media/img/" + card1 + ".jpg"
-                card2_url = "media/img/" + card2 + ".jpg"
-                //$("#card-id").html(card1_url + " " +  card2)
-                $("#card1").attr("src", card1_url);
-                $("#card2").attr("src", card2_url);
+            success: function(response) {
+                current_user_name = response;
+                if (current_user_name == user_name) {
+                    // username ophalen uit current_player.json
+                    $.ajax({
+                        url: 'scripts/start_game.php',
+                        method: 'GET',
+                        success: function(response){
+                            card1 = response.split(" ")[0];
+                            card2 = response.split(" ")[1];
+                            card1_url = "media/img/" + card1 + ".jpg"
+                            card2_url = "media/img/" + card2 + ".jpg"
+                            //$("#card-id").html(card1_url + " " +  card2)
+                            $("#card1").attr("src", card1_url);
+                            $("#card2").attr("src", card2_url);
+                        }
+                    });
+                }
             }
         });
     });
-}
+}co
 
 function update_current_player() {
     $.ajax({
@@ -87,29 +98,43 @@ function player_name_display() {
 
 function more_cards() {
     $('#more-cards').click(function(event) {
+        event.preventDefault();
+        current_user_name = "";
+        user_name = sessionStorage.getItem('player-name');
         $.ajax({
-            url: "scripts/extra_card.php",
-            method: "GET",
+            url: 'scripts/load_current_user.php',
+            method: 'GET',
             success: function(response) {
-                new_card = response;
-                new_card_url = "media/img/" + new_card + ".jpg"
-                if ($("#card3").attr("src") != default_card_img) {
-                    //alert("card 3 is used already");
-                    if ($("#card4").attr("src") != default_card_img) {
-                        //alert("card 4 is used already");
-                        if ($("#card5").attr("src") != default_card_img) {
-                            //alert("cards are full");
+                current_user_name = response;
+                if (current_user_name == user_name) {
+                    // username ophalen uit current_player.json
+                    $.ajax({
+                        url: "scripts/extra_card.php",
+                        method: "GET",
+                        success: function(response) {
+                        new_card = response;
+                        new_card_url = "media/img/" + new_card + ".jpg"
+                        if ($("#card3").attr("src") != default_card_img) {
+                            //alert("card 3 is used already");
+                            if ($("#card4").attr("src") != default_card_img) {
+                                //alert("card 4 is used already");
+                                if ($("#card5").attr("src") != default_card_img) {
+                                    //alert("cards are full");
+                                } else {
+                                    $("#card5").attr("src", new_card_url);
+                                }
+                            } else {
+                                $("#card4").attr("src", new_card_url);
+                            }
                         } else {
-                            $("#card5").attr("src", new_card_url);
+                            $("#card3").attr("src", new_card_url);
                         }
-                    } else {
-                        $("#card4").attr("src", new_card_url);
-                    }
-                } else {
-                    $("#card3").attr("src", new_card_url);
+                        }
+                    });
                 }
             }
-        })
+        });
+        
     })
 }
 
