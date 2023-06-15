51d519524b1920e55358bf8cb7f9414fe9437028
Jelmer <jelmersmit@upcmail.nl>
2023-06-15 17:22:05 +0200

    Added reset of data.json to init script

--- scripts/init.php
+++ scripts/init.php
@@ -9,4 +9,8 @@
     $cur_play_file = fopen("../data/current_player.json", "w");
     fwrite($cur_play_file, "1");
     
+    // Reset data.json
+    $datafile = fopen("../data/data.json", "w");
+    fwrite($datafile, '[{"id":0,"name":"Admin","status":"inactive"}]');
+    
 ?>
