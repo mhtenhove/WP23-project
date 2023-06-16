e48004a0c48ec2f7d95128a6053075cd578ecf35
Jelmer <jelmersmit@upcmail.nl>
2023-06-16 14:34:14 +0200

    Fixed player_turn.php

--- scripts/player_turn.php
+++ scripts/player_turn.php
@@ -17,16 +17,13 @@ $scores = json_decode($score_json, true);
 // check if round scores exist and otherwise initialize
 $roundscorefile = file_get_contents("../data/round_scores.json");
 $roundscores = json_decode($roundscorefile);
-p_print($scores);
 if ($current_player_id == $scores[0]) {
     array_push($roundscores, [$current_player_id, $scores[1]]);
 }
-p_print($roundscores);
 // we assume $round_scores is an array scores, one for each player, so first element is player 1 etc.
 
 $roundscorefile2 = fopen("../data/round_scores.json", "w");
 fwrite($roundscorefile2, json_encode($roundscores));
-
 
 $players = json_decode($json_file2, true);
 $player_count = count($players) - 1;
@@ -41,8 +38,16 @@ if ($current_player_id < $player_count) {
     $updated_player = json_encode($current_player);
     $cur_play_file = fopen("../data/current_player.json", "w");
     fwrite($cur_play_file, $updated_player);
+    $best_player = 0;
+    for ($x = 0; $x < count($roundscores); $x++) {
+        $best_player_score = 0;
+        if ($roundscores[$x][1] > $best_player_score) {
+            $best_player = $roundscores[$x][0];
+            $best_player_score = $roundscores[$x][1];
+        }
+    }
+    if ($best_player != 0) {
+        echo $best_player;
+    }
 }
-
-p_print($current_player_id);
-
 ?>
